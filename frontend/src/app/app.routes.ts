251253f110b7e929928ef4039a6c939ecfa03a8d
Italo Oliveira <italooliveiraxdd@gmail.com>
2026-03-29 11:48:33 -0300

    feat: implementa tela de login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: LoginComponent }, // Temporário, até criar o RegisterComponent
+    { path: 'dashboard', component: LoginComponent }, // Temporário, até criar o DashboardComponent
+    { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
